1c93734946137fa62e168614f6c7ed2776cfd309
resat <resat.gafarli@gmail.com>
2017-06-06 17:56:03 +0300

    Unit test dependend class templetizing example has been implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,64 @@
 #include <iostream>
+#include "gmock/gmock.h"
 
-#include "gmock/gmock.h"  // Brings in Google Mock.
+/*-------------------------------- The class ---------------------------------------*/
+struct AIntr {
+    virtual int getVal() = 0;
+};
 
-struct A {
-    A():val(10){
-
-    }
+struct A: public AIntr {
+    A():val(10){}
     int getVal(){
         return val;
     }
+
 private:
     int val;
+
 };
 
+/*---------------------------- The depended class -----------------------------------*/
+struct BIntr{
+    virtual int getVal() = 0;
+};
 
-struct B{
-
-    A b;
-
-    int getBVal(){
-        return b.getVal();
+template<typename T>
+struct BTemp: public BIntr{
+    BTemp(T& dependenClass):t(dependenClass){}
+    int getVal(){
+       int v =t.getVal();
+       return ++v;
     }
-
+private:
+    T &t;
 };
 
-struct MockB: public B{
-    MOCK_METHOD0(getBVal,int());
+
+/*------------------------ The mock objects -------------------------------*/
+struct MockA: public AIntr{
+    MOCK_METHOD0(getVal,int());
 };
 
-TEST(ExampleMocTest, DependencyMocking) {
+/*------------------------ Tests --------------------------*/
 
-    MockB b;
-    EXPECT_EQ(10,b.getBVal());
 
+
+
+TEST(AClassTest, GetAClassValue) {
+    A a;
+    EXPECT_EQ(10,a.getVal());
 }
 
+using ::testing::Return;
+TEST(BClassTest, GetBClassValue){
+    MockA mockA;
+    EXPECT_CALL(mockA,getVal()).WillRepeatedly(Return(100));
+
+    //Production code declaration is "BTemp<A> B(realDependenObjectRefernece);"
+    BTemp<MockA> b(mockA);
+
+    EXPECT_EQ(101,b.getVal());
+}
 
 int main(int argc, char * argv[]){
   ::testing::InitGoogleMock(&argc, argv);

c4df37b41144eb1005eda6bda668916b33987b47
resat <resat.gafarli@gmail.com>
2017-06-06 16:44:13 +0300

    Exampl class depenedency.

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,39 @@
 #include <iostream>
-class Turtle {
-public:
-  virtual ~Turtle() {}
-  virtual void PenUp() = 0;
-  virtual void PenDown() = 0;
-  virtual void Forward(int distance) = 0;
-  virtual void Turn(int degrees) = 0;
-  virtual void GoTo(int x, int y) = 0;
-  virtual int GetX() const = 0;
-  virtual int GetY() const = 0;
-};
 
 #include "gmock/gmock.h"  // Brings in Google Mock.
-using ::testing::AtLeast;  
-class MockTurtle : public Turtle {
- public:
-  MOCK_METHOD0(PenUp, void());
-  MOCK_METHOD0(PenDown, void());
-  MOCK_METHOD1(Forward, void(int distance));
-  MOCK_METHOD1(Turn, void(int degrees));
-  MOCK_METHOD2(GoTo, void(int x, int y));
-  MOCK_CONST_METHOD0(GetX, int());
-  MOCK_CONST_METHOD0(GetY, int());
+
+struct A {
+    A():val(10){
+
+    }
+    int getVal(){
+        return val;
+    }
+private:
+    int val;
 };
 
-TEST(PainterTest, CanDrawSomething) {
-	MockTurtle turtle;                          // #2
-	EXPECT_CALL(turtle, PenDown())              // #3
-      .Times(AtLeast(1));
 
-	//Painter painter(&turtle);                   // #4
+struct B{
 
-	//EXPECT_TRUE(painter.DrawCircle(0, 0, 10));
-}                                             // #5
+    A b;
+
+    int getBVal(){
+        return b.getVal();
+    }
+
+};
+
+struct MockB: public B{
+    MOCK_METHOD0(getBVal,int());
+};
+
+TEST(ExampleMocTest, DependencyMocking) {
+
+    MockB b;
+    EXPECT_EQ(10,b.getBVal());
+
+}
 
 
 int main(int argc, char * argv[]){

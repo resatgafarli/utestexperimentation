7cb9d43284cea3aa806e74612d392421b43f3861
Resat <resat.gafarli@gmail.com>
2017-06-07 21:54:20 +0300

    Ldaf test unit has been added to project configuraiton.

---
+++ src/tst_ldaf.cpp
@@ -0,0 +1,8 @@
+/******************************************************
+Author: resat.gafarli@gmail.com
+File: tst_ldaf.cpp
+Date: 6/7/2017
+License: GPL-3.0
+*******************************************************/
+#include "tst_ldaf.h"
+
---
+++ src/tst_ldaf.h
@@ -0,0 +1,12 @@
+/******************************************************
+Author: resat.gafarli@gmail.com
+File: tst_ldaf.h
+Date: 6/7/2017
+License: GPL-3.0
+*******************************************************/
+#ifndef TST_LDAF_H
+#define TST_LDAF_H
+
+
+
+#endif // TST_LDAF_H

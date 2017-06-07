77d41a2072883785f932ede2f4b899f62cf0076f
Resat <resat.gafarli@gmail.com>
2017-06-07 22:09:54 +0300

    tst_ldaf.cpp has been removed.

--- src/tst_ldaf.cpp
+++
@@ -1,8 +0,0 @@
-/******************************************************
-Author: resat.gafarli@gmail.com
-File: tst_ldaf.cpp
-Date: 6/7/2017
-License: GPL-3.0
-*******************************************************/
-#include "tst_ldaf.h"
-
